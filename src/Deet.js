fdbf0d8123ba9412904c4d3c9d77b5463d680892
cloud-nate <60197607+cloud-nate@users.noreply.github.com>
2020-02-21 14:31:53 -0600

    Add files via upload
    init commit

---
+++ src/Deet.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function Deets() {
+
+    return (
+        <div>
+            <title>Photos:BigFish</title>
+            <div className="start-template">
+                <div className="starter-template">
+                    <h2>Details about a photo</h2>
+                    <br />
+                    <p className="lead">Below is a description of a photo of a little goldfish.</p>
+                    <br />
+                    <a href="deets"><img src="imgs/goldfsh.jpg" alt=" Goldfish photo one" height="100%" width="100%" /></a>
+                    <p> <br />
+                        This goldfish is not gold in color. <br />
+                        File Type: JPG File <br />
+                        File Size: 28 KB <br />
+                        File Dimensions: 675 x 450 px
+                    </p>
+                </div>
+            </div>
+            <p>Created by Nate V: 2/21/2020</p>
+        </div >
+    );
+}
+
+export default Deets;
